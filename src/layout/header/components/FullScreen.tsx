39bda162ce7e110229f05292c3f2c8c87a9d1fe6
吧啦哔哩啵 <1011831550@qq.com>
2025-05-10 15:16:47 +0800

    perf: 优化了全屏显示提示

--- src/layout/header/components/FullScreen.tsx
+++ src/layout/header/components/FullScreen.tsx
@@ -1,15 +1,27 @@
 import { FullscreenExitOutlined, FullscreenOutlined } from '@ant-design/icons'
 import { Tooltip } from 'antd'
 import { useFullscreen } from 'ahooks'
+import { useState } from 'react'
 
 const FullScreen: React.FC = () => {
   const [isFullscreen, { toggleFullscreen }] = useFullscreen(
     () => document.documentElement
   )
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    toggleFullscreen()
+    setIsOpen(false)
+  }
 
   return (
-    <Tooltip placement='bottom' title={isFullscreen ? '退出全屏' : '全屏'}>
-      <div className='cursor-pointer' onClick={toggleFullscreen}>
+    <Tooltip
+      placement='bottom'
+      open={isOpen}
+      title={isFullscreen ? '退出全屏' : '全屏'}
+      onOpenChange={setIsOpen}
+    >
+      <div className='cursor-pointer' onClick={handleClick}>
         {isFullscreen ? <FullscreenExitOutlined /> : <FullscreenOutlined />}
       </div>
     </Tooltip>
