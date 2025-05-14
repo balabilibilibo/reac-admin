13c204367d1267867803970f1933fa25f7f47bfc
吧啦哔哩啵 <1011831550@qq.com>
2025-05-14 23:08:35 +0800

    feat: 路由边界处理

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -9,6 +9,7 @@ import Menu12 from '@/views/menu/menu1/menu1-2'
 import Menu21 from '@/views/menu/menu2/menu2-1'
 import Menu221 from '@/views/menu/menu2/menu2-2/menu2-2-1'
 import NotFound from '@/views/Exception/NotFound'
+import ErrorBoundary from '@/views/Exception/ErrorBoundary'
 
 import About from '@/views/about'
 
@@ -22,72 +23,85 @@ const constRoutes: RouteObject[] = [
     Component: Login,
   },
   {
-    path: '/dashboard',
+    path: '',
     element: <AuthGuard children={<LayoutCon />} />,
+    ErrorBoundary: ErrorBoundary,
     children: [
       {
-        path: 'analysis',
-        element: <Analysis />,
-      },
-      {
-        path: 'workbench',
-        element: <Workbench />,
-      },
-    ],
-  },
-  {
-    path: '/menu',
-    element: <AuthGuard children={<LayoutCon />} />,
-    children: [
-      {
-        path: 'menu1',
+        path: '/dashboard',
         children: [
           {
-            path: 'menu1-1',
-            element: <Menu11 />,
+            path: 'analysis',
+            element: <Analysis />,
           },
           {
-            path: 'menu1-2',
-            element: <Menu12 />,
+            path: 'workbench',
+            element: <Workbench />,
           },
         ],
       },
       {
-        path: 'menu2',
+        path: '/menu',
         children: [
           {
-            path: 'menu2-1',
-            element: <Menu21 />,
-          },
-          {
-            path: 'menu2-2',
+            path: 'menu1',
             children: [
               {
-                path: 'menu2-2-1',
-                element: <Menu221 />,
+                path: 'menu1-1',
+                element: <Menu11 />,
+              },
+              {
+                path: 'menu1-2',
+                element: <Menu12 />,
+              },
+            ],
+          },
+          {
+            path: 'menu2',
+            children: [
+              {
+                path: 'menu2-1',
+                element: <Menu21 />,
+              },
+              {
+                path: 'menu2-2',
+                children: [
+                  {
+                    path: 'menu2-2-1',
+                    element: <Menu221 />,
+                  },
+                ],
               },
             ],
           },
         ],
       },
-    ],
-  },
-  {
-    path: '/about',
-    element: <AuthGuard children={<LayoutCon />} />,
-    children: [
       {
-        index: true,
-        element: <About />,
+        path: '/about',
+        children: [
+          {
+            index: true,
+            // loader: async () => {
+            //   //throw new Response('Not Found', { status: 404, statusText: 'Not Found' }); 可以返回Response对象
+            //   //也可以返回json等等
+            //   throw {
+            //     message: 'Not Found',
+            //     status: 404,
+            //     statusText: 'Not Found',
+            //     data: '132131',
+            //   }
+            // },
+            element: <About />,
+          },
+        ],
       },
-    ],
-  },
-  {
-    element: <LayoutCon />,
-    children: [
       {
-        path: '*',
-        element: <NotFound />,
+        children: [
+          {
+            path: '*',
+            element: <NotFound />,
+          },
+        ],
       },
     ],
   },
---
+++ src/views/Exception/ErrorBoundary.tsx
@@ -0,0 +1,31 @@
+import { Button } from 'antd'
+import { useNavigate, useRouteError } from 'react-router-dom'
+import errorSrc from '@/assets/error.svg'
+
+const ErrorBoundary = () => {
+  const error = useRouteError()
+  console.log(
+    'useRouteError 用于获取在ActionFunction、LoaderFunction 或组件渲染期间抛出的错误，用于路由模块的错误边界',
+    error
+  )
+  const navigate = useNavigate()
+  const goHome = () => {
+    navigate('/dashboard/analysis')
+  }
+  return (
+    <div className='flex items-center justify-center h-full bg-white dark:bg-black flex flex-col text-[#1f1f1f] dark:text-[#d9d9d9]'>
+      <img width={400} src={errorSrc} title='抱歉你访问的页面不存在哦' />
+      <div className='text-center mt-[-80px]'>
+        <div className='text-2xl'>500</div>
+        <div className='text-black/45 dark:text-white/45 my-5'>
+          抱歉哦，系统故障，请联系管理员！
+        </div>
+        <Button type='primary' onClick={goHome}>
+          返回首页
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default ErrorBoundary
