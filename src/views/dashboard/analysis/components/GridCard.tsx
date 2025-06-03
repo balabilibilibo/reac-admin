1bf2cd470052746f0e7dc13bb57e71780f0adb9c
吧啦哔哩啵 <1011831550@qq.com>
2025-06-03 22:43:50 +0800

    feat: :sparkles: 统计数值动画

--- src/views/dashboard/analysis/components/GridCard.tsx
+++ src/views/dashboard/analysis/components/GridCard.tsx
@@ -1,25 +1,20 @@
-import { Card, Tag } from 'antd'
+import { Card, Statistic, StatisticProps, Tag } from 'antd'
 import { growCardList } from '../data'
 import { SvgIcon } from '@/components/Icon/SvgIcon'
-
+import CountUp from 'react-countup'
+const formatter: StatisticProps['formatter'] = (value) => <CountUp end={value as number} separator="," />
 export default function GridCard() {
   return (
     <div className="grid grid-cols-4 gap-2.5">
       {growCardList.map((item, index) => (
-        <Card
-          key={index}
-          size="small"
-          className=""
-          title={item.title}
-          extra={<Tag color={item.color}>{item.action}</Tag>}
-        >
+        <Card key={index} size="small" title={item.title} extra={<Tag color={item.color}>{item.action}</Tag>}>
           <div className="flex items-center justify-between p-2">
-            <span className="text-lg">{item.value}</span>
+            <Statistic prefix="$" valueStyle={{ fontSize: '18px' }} value={item.value} formatter={formatter} />
             <SvgIcon icon={item.icon} size={40} />
           </div>
           <div className="flex justify-between p-2">
             <span>总{item.title}</span>
-            <span>{item.total}</span>
+            <Statistic prefix="$" valueStyle={{ fontSize: '16px' }} value={item.total} formatter={formatter} />
           </div>
         </Card>
       ))}
