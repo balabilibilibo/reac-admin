db32bf0c2c90e162bc0cae4c7bca1fa02e8a063c
吧啦哔哩啵 <1011831550@qq.com>
2025-06-06 16:57:47 +0800

    feat: :sparkles: 回到顶部

--- src/layout/content/index.tsx
+++ src/layout/content/index.tsx
@@ -1,25 +1,28 @@
-import { Layout, theme } from 'antd'
+import { Layout, theme, FloatButton } from 'antd'
 import { Outlet } from 'react-router-dom'
-import { Suspense } from 'react'
+import { Suspense, useRef } from 'react'
 import { ProgressBar } from '@/components/ProgressBar'
 const Content: React.FC = () => {
+  const contnetRef = useRef<HTMLDivElement>(null)
   const {
     token: { borderRadiusLG }
   } = theme.useToken()
   return (
     <Layout.Content
       className="overflow-y-auto bg-[#f5f5f5] dark:bg-black"
+      ref={contnetRef}
       style={{
         margin: 10,
         minHeight: 280,
         borderRadius: borderRadiusLG
       }}
     >
-      <div className="h-full">
+      <div className="relative h-full">
         <Suspense fallback={<div>Loading...</div>}>
           <ProgressBar />
           <Outlet />
         </Suspense>
+        <FloatButton.BackTop visibilityHeight={100} target={() => contnetRef.current!} />
       </div>
     </Layout.Content>
   )
