e73c9a6bd23de01e4ba2b00fad68657f90150eab
吧啦哔哩啵 <1011831550@qq.com>
2025-06-12 22:44:47 +0800

    style: :lipstick: 消除路由切换动画产生的滚动条

--- src/layout/content/index.tsx
+++ src/layout/content/index.tsx
@@ -20,7 +20,7 @@ const Content: React.FC = () => {
         borderRadius: borderRadiusLG
       }}
     >
-      <TransitionGroup className="h-full">
+      <TransitionGroup className="h-full overflow-x-hidden">
         <CSSTransition nodeRef={nodeRef} key={location.pathname} timeout={300} classNames="fade">
           <div ref={nodeRef} className="h-full">
             <Suspense fallback={<div>Loading...</div>}>
