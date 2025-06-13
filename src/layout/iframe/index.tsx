3d4d972ec4f5e5392c3a515ace84e76ac5e2d35c
吧啦哔哩啵 <1011831550@qq.com>
2025-06-13 13:39:42 +0800

    feat: :sparkles: 添加内嵌网页加载动画

--- src/layout/iframe/index.tsx
+++ src/layout/iframe/index.tsx
@@ -1,6 +1,9 @@
 import { usePermissionStore } from '@/store/permission'
 import { transformToMenu } from '@/utils/menu'
 import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
+import { Spin } from 'antd'
+import './index.less'
 function findNode(list: any[], target: string): any | null {
   for (const item of list) {
     if (item.path === target) {
@@ -17,10 +20,17 @@ function findNode(list: any[], target: string): any | null {
 }
 
 export default function IFrame() {
+  const [loading, setLoading] = useState(true)
   const { backMenuList } = usePermissionStore()
   const menuList = transformToMenu(backMenuList)
   const { pathname } = useLocation()
   const node = findNode(menuList, pathname)
   if (!node) return null
-  return <iframe src={node.frameSrc} style={{ width: '100%', height: '100%' }}></iframe>
+  return (
+    <div className="iframe-page">
+      <Spin spinning={loading} wrapperClassName="h-full">
+        <iframe src={node.frameSrc} style={{ width: '100%', height: '100%' }} onLoad={() => setLoading(false)}></iframe>
+      </Spin>
+    </div>
+  )
 }
